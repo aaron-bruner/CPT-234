d39444c1c9067f7e389ce0a4ea8d23b48533edc4
SkittlezExZ <skittlezexz@gmail.com>
2020-02-25 14:49:34 -0500

    Lecture 02/25/20

---
+++ chapter12/12_1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+#define YES 1
+#define NO 0
+
+// Function to determine if an integer is even
+
+int isEven (int number) {
+
+    int answer;
+
+    if (number % 2 == 0)
+        answer = YES;
+    else
+        answer = NO;
+
+    return answer;
+}
+
+int main(void){
+
+    int isEven (int number);
+
+    if (isEven (17) == YES)
+        printf("Yes ");
+    else
+        printf("No ");
+
+    if (isEven (20)== YES)
+        printf("Yes \n");
+    else
+        printf("No \n");
+
+    return 0;
+
+}
