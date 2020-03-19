24d56cc508b017eb141fb7148bbb740c2bf51506
SkittlezExZ <skittlezexz@gmail.com>
2020-03-19 17:01:07 -0400

    Project 3

---
+++ prog/prog3/prog3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+
+int processLED (int bulbNum);
+int processLumens(int bulbNum,
+
+
+
+
+
+int main () {
+
+    int month, day, year;
+    
+    scanf("%d%d%d", &month,&day,&year);
+
+    
+    printf("Test date: %d/%d/%d\nLED Lumens\n", month, day, year);
+
+    while (counter != -1) {
+    processLED();
+
+    }
+
+
+return 0;
+
+}
